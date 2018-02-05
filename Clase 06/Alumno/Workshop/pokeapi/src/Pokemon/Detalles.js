2536f56fe981a1a63b53e30516dcf49e129632ad
nehuen <onamonzon.32@gmail.com>
2018-02-04 21:37:48 -0300

    add . "changes"

--- Clase 06/Alumno/Workshop/pokeapi/src/Pokemon/Detalles.js
+++ Clase 06/Alumno/Workshop/pokeapi/src/Pokemon/Detalles.js
@@ -15,7 +15,6 @@ function Detalles({ id, name, height, weight, sprites }) {
     <li style={styles.li}>
       <img style={styles.img} src={sprites.front_default} />
       <dl>{`# ${id} ${name}`}</dl>
-      <dl>{`${weight}`}</dl>
       <dl> {`Peso: ${height}`}</dl>
       <dl> {`Altura:${height}`}</dl>
     </li>
