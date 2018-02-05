d4d3e7646799e5441acab1346056ecc02eacec1a
nehuen <onamonzon.32@gmail.com>
2018-02-04 21:46:54 -0300

    add "Desafio6"

--- Clase 06/Alumno/Workshop/pokeapi/src/Pokemon/Detalles.js
+++ Clase 06/Alumno/Workshop/pokeapi/src/Pokemon/Detalles.js
@@ -4,7 +4,11 @@ const styles = {
   li: {
     textTransform: 'capitalize',
     fontSize: 24,
-    color: red
+    color: 'red',
+    borderRadius: 9,
+    borderColor: 'black',
+    borderStyle: 'solid',
+    margin: 0
   },
   img: {
     width: 200
